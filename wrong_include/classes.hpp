82eba8ee03342604b02b1f6db595f7245d4c256a
fakeshine <mihnea.apostolescu@yahoo.com>
2022-12-12 09:51:38 +0200

    Moved Student class from main to classes.hpp

--- wrong_include/classes.hpp
+++ wrong_include/classes.hpp
@@ -2,6 +2,74 @@
 
 // TODO move Student here
 
+class Student
+{
+    private:
+        int id;
+        std::string name;
+        std::vector<double> grades;
+        double average;
+
+    public:
+        Student(int id, std::string name, std::vector<double> grades);
+
+        int getId();
+        std::string getName();
+        std::vector<double> getGrades();
+        double getAverage();
+        
+        void calculateAverage(); // TODO: implement this function
+        bool hasFailed();
+
+        friend std::ostream& operator<<(std::ostream& out, const Student& student);
+};
+
+Student::Student(int id, std::string name, std::vector<double> grades)
+{
+    this->id = id;
+    this->name = name;
+    this->grades = grades;
+}
+
+int Student::getId()
+{
+    return this->id;
+}
+
+std::string Student::getName()
+{
+    return this->name;
+}
+
+std::vector<double> Student::getGrades()
+{
+    return this->grades;
+}
+
+double Student::getAverage()
+{
+    return this->average;
+}
+
+bool Student::hasFailed()
+{
+    return this->average > 5.0;
+}
+
+std::ostream& operator<<(std::ostream& out, const Student& student)
+{
+    out << "ID: " << student.id << "\n" << "Name: " << student.name << "\n" << "Grades: [ ";
+
+    for(double i : student.grades)
+    {
+        out << i << " ";
+    }
+
+    out << "]\n" << "Average: " << student.average << "\n";
+
+    return out;
+}
+
 class University
 {
     protected:
